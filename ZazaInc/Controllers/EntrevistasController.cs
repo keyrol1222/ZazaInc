03ebaac614b8dc769fe59fa595489b961e5779a0
keyrol <20186176@itla.edu.do>
2020-11-13 15:49:44 -0800

    añadi los crud iniciales osea cada pestana con sus tablas

---
+++ ZazaInc/Controllers/EntrevistasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ZazaInc.Models;
+
+namespace ZazaInc.Controllers
+{
+    public class EntrevistasController : Controller
+    {
+        private DBZazaIncEntities db = new DBZazaIncEntities();
+
+        // GET: Entrevistas
+        public ActionResult Index()
+        {
+            var entrevistas = db.Entrevistas.Include(e => e.Solicitante).Include(e => e.Usuario);
+            return View(entrevistas.ToList());
+        }
+
+        // GET: Entrevistas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entrevista entrevista = db.Entrevistas.Find(id);
+            if (entrevista == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entrevista);
+        }
+
+        // GET: Entrevistas/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id_solicitante = new SelectList(db.Solicitantes, "id_solicitante", "nombre_solicitante");
+            ViewBag.Id_usuario = new SelectList(db.Usuarios, "id_usuario", "nombre_usuario");
+            return View();
+        }
+
+        // POST: Entrevistas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_Entrevista,Id_usuario,Id_solicitante,notas")] Entrevista entrevista)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entrevistas.Add(entrevista);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id_solicitante = new SelectList(db.Solicitantes, "id_solicitante", "nombre_solicitante", entrevista.Id_solicitante);
+            ViewBag.Id_usuario = new SelectList(db.Usuarios, "id_usuario", "nombre_usuario", entrevista.Id_usuario);
+            return View(entrevista);
+        }
+
+        // GET: Entrevistas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entrevista entrevista = db.Entrevistas.Find(id);
+            if (entrevista == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id_solicitante = new SelectList(db.Solicitantes, "id_solicitante", "nombre_solicitante", entrevista.Id_solicitante);
+            ViewBag.Id_usuario = new SelectList(db.Usuarios, "id_usuario", "nombre_usuario", entrevista.Id_usuario);
+            return View(entrevista);
+        }
+
+        // POST: Entrevistas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_Entrevista,Id_usuario,Id_solicitante,notas")] Entrevista entrevista)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(entrevista).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id_solicitante = new SelectList(db.Solicitantes, "id_solicitante", "nombre_solicitante", entrevista.Id_solicitante);
+            ViewBag.Id_usuario = new SelectList(db.Usuarios, "id_usuario", "nombre_usuario", entrevista.Id_usuario);
+            return View(entrevista);
+        }
+
+        // GET: Entrevistas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entrevista entrevista = db.Entrevistas.Find(id);
+            if (entrevista == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entrevista);
+        }
+
+        // POST: Entrevistas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Entrevista entrevista = db.Entrevistas.Find(id);
+            db.Entrevistas.Remove(entrevista);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ZazaInc/Controllers/TerminarsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ZazaInc.Models;
+
+namespace ZazaInc.Controllers
+{
+    public class TerminarsController : Controller
+    {
+        private DBZazaIncEntities db = new DBZazaIncEntities();
+
+        // GET: Terminars
+        public ActionResult Index()
+        {
+            var terminars = db.Terminars.Include(t => t.Empleado).Include(t => t.Usuario);
+            return View(terminars.ToList());
+        }
+
+        // GET: Terminars/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Terminar terminar = db.Terminars.Find(id);
+            if (terminar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(terminar);
+        }
+
+        // GET: Terminars/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_empleado = new SelectList(db.Empleadoes, "id_empleado", "nombre_empleado");
+            ViewBag.id_usuario = new SelectList(db.Usuarios, "id_usuario", "nombre_usuario");
+            return View();
+        }
+
+        // POST: Terminars/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_Terminar,id_usuario,id_empleado,razon")] Terminar terminar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Terminars.Add(terminar);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_empleado = new SelectList(db.Empleadoes, "id_empleado", "nombre_empleado", terminar.id_empleado);
+            ViewBag.id_usuario = new SelectList(db.Usuarios, "id_usuario", "nombre_usuario", terminar.id_usuario);
+            return View(terminar);
+        }
+
+        // GET: Terminars/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Terminar terminar = db.Terminars.Find(id);
+            if (terminar == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_empleado = new SelectList(db.Empleadoes, "id_empleado", "nombre_empleado", terminar.id_empleado);
+            ViewBag.id_usuario = new SelectList(db.Usuarios, "id_usuario", "nombre_usuario", terminar.id_usuario);
+            return View(terminar);
+        }
+
+        // POST: Terminars/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_Terminar,id_usuario,id_empleado,razon")] Terminar terminar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(terminar).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_empleado = new SelectList(db.Empleadoes, "id_empleado", "nombre_empleado", terminar.id_empleado);
+            ViewBag.id_usuario = new SelectList(db.Usuarios, "id_usuario", "nombre_usuario", terminar.id_usuario);
+            return View(terminar);
+        }
+
+        // GET: Terminars/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Terminar terminar = db.Terminars.Find(id);
+            if (terminar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(terminar);
+        }
+
+        // POST: Terminars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Terminar terminar = db.Terminars.Find(id);
+            db.Terminars.Remove(terminar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
