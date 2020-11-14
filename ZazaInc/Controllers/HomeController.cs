90be7c9c928a439c46667e17ff9cb82528cb6cf8
keyrol <20186176@itla.edu.do>
2020-11-13 22:52:40 -0800

    login

--- ZazaInc/Controllers/HomeController.cs
+++ ZazaInc/Controllers/HomeController.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing.Printing;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using ZazaInc.Models;
 
 namespace ZazaInc.Controllers
 {
@@ -13,18 +20,53 @@ namespace ZazaInc.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+
+        [HttpPost]
+        public ActionResult Login(String correo_usuario, String contraseña_usuario)
+        {
+
+            if (!string.IsNullOrEmpty(correo_usuario) && !string.IsNullOrEmpty(contraseña_usuario))
+            {
+
+                DBZazaIncEntities dB = new DBZazaIncEntities();
+                var user = dB.Usuarios.FirstOrDefault(e => e.correo_usuario == correo_usuario && e.contraseña_usuario == contraseña_usuario);
+
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(user.correo_usuario, true);
+                    
+
+                    
+                    
+
+
+                        return RedirectToAction("Index", "Home");
+                    
+
+
+                }
+                else
+                {
+                    return RedirectToAction("Index", new { Message = "no se encontro" });
+                }
+            }
+
+            else
+            {
+                return RedirectToAction("Index", new { Message = "LLene los cuadros" });
+            }
+
+
         }
 
-        public ActionResult Contact()
+        [Authorize]
+        public ActionResult Logout()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
         }
-    }
+    
+}
+
 }
