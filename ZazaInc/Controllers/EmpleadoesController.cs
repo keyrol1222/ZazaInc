cc1d0906da51ce377804e67b91a2d9a714af503e
keyrol <20186176@itla.edu.do>
2020-11-24 11:46:45 -0800

    export csv

--- ZazaInc/Controllers/EmpleadoesController.cs
+++ ZazaInc/Controllers/EmpleadoesController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using ZazaInc.Models;
@@ -104,6 +105,32 @@ namespace ZazaInc.Controllers
             }
             return View(empleado);
         }
+        public void ExportCSV_Employee()
+        {
+            var sb = new StringBuilder();
+            // You can write sql query according your need  
+            string qry = ("Select * from Empleado");
+            IEnumerable<Empleado> query = db.Database.SqlQuery<Empleado>(qry);
+            var list = query.ToList();
+
+            sb.AppendFormat("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}", "id", "nombre", "apellido", "posicion", "cedula", "correo", "pago", "horario", "departamento", Environment.NewLine);
+            foreach (var item in list)
+            {
+                sb.AppendFormat("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}", item.id_empleado, item.nombre_empleado, item.apellido_empleado, item.posicion_empleado, item.cedula_empleado, item.correo_usuario, item.pago_empleado,item.horario_empleado,item.departamento, Environment.NewLine);
+            }
+            //Get Current Response  
+            var response = System.Web.HttpContext.Current.Response;
+            response.BufferOutput = true;
+            response.Clear();
+            response.ClearHeaders();
+            response.ContentEncoding = Encoding.Unicode;
+            response.AddHeader("content-disposition", "attachment;filename=Employee.CSV ");
+            response.ContentType = "text/plain";
+            response.Write(sb.ToString());
+            response.End();
+        }
+
+
 
         // POST: Empleadoes/Delete/5
         [HttpPost, ActionName("Delete")]
